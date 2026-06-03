4cf486a55ba65b3f74de5e0cc779ce674c15e713
ettico <etti0475@gmail.com>
2026-06-03 23:09:58 +0300

    vite

--- ReactProject/vite.config.ts
+++ ReactProject/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // מגדיר גבול גבוה יותר לאזהרות כדי שה-Build לא יכשל על גודל קבצים
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // מפצל את הקוד לחלקים קטנים יותר כדי להקל על השרת
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
